97c2a0c6ddb4f3f5686f0e286cfc2e981270c536
asmita143 <89544722+asmita143@users.noreply.github.com>
2025-01-30 16:12:03 +0200

    Form created for admin

--- vite-project/src/Login.tsx
+++ vite-project/src/Login.tsx
@@ -1,8 +1,8 @@
 // src/Login.tsx
 
-import React,  { useState } from "react";
-import { useNavigate } from "react-router-dom";  
-import { auth } from "./firebase"; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
 
@@ -11,7 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,7 +23,7 @@ const Login = () => {
       );
       const user = userCredential.user;
       console.log("Logged in as:", user.email);
-      navigate("/home");  
+      navigate("/home");
     } catch (error: unknown) {
       if (error instanceof Error) {
         setError(error.message);
@@ -34,71 +34,81 @@ const Login = () => {
   };
 
   return (
-<div className="h-screen w-screen flex bg-gradient-to-r from-green-900 to-green-400">
-  {/* Left Section */}
-  <div className="w-2/3 flex flex-col justify-center items-center text-white">
-    <h1 className="text-4xl font-bold py-3">"Capture Moments, Keep Memories."</h1>
-  </div>
+    <div className="h-screen w-screen flex bg-gradient-to-r from-green-900 to-green-400">
+      {/* Left Section */}
+      <div className="w-2/3 flex flex-col justify-center items-center text-white">
+        <h1 className="text-4xl font-bold py-3">
+          "Capture Moments, Keep Memories."
+        </h1>
+      </div>
 
-  {/* Right Section */}
-  <div className="w-1/3 flex justify-center items-center bg-gray-100">
-    <div className="bg-white shadow-lg rounded-lg p-8 flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold mb-1">KivaPix</h1>
-      <p className="text-gray-500 mb-6">Login in to your account to continue</p>
-      <form onSubmit={handleLogin} className="w-96 space-y-4">
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-            className="w-full p-3 border border-gray-300 rounded-md"
-          />
+      {/* Right Section */}
+      <div className="w-1/3 flex justify-center items-center bg-gray-100">
+        <div className="bg-white shadow-lg rounded-lg p-8 flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold mb-1">KivaPix</h1>
+          <p className="text-gray-500 mb-6">
+            Login in to your account to continue
+          </p>
+          <form onSubmit={handleLogin} className="w-96 space-y-4">
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+                className="w-full p-3 border border-gray-300 rounded-md"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+                className="w-full p-3 border border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="flex justify-between pt-2">
+              <div className="flex items-center space-x-3">
+                <input type="checkbox" id="terms" className="w-5 h-5" />
+                <p>Remember me</p>
+              </div>
+              <div>
+                <a href="#" className="text-blue-500 items-end">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-center py-3">
+              <button
+                type="submit"
+                className="w-full py-3 bg-green-500 text-white font-bold rounded-3xl text-xl"
+              >
+                Login
+              </button>
+              <div className="py-3">
+                <label htmlFor="terms" className="text-gray-700">
+                  Don't have an account?{" "}
+                  <Link to="/register" className="text-blue-500 underline">
+                    Create account
+                  </Link>
+                </label>
+              </div>
+            </div>
+          </form>
+          {error && <p className="text-red-500 mt-4">{error}</p>}
         </div>
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-            className="w-full p-3 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div className="flex justify-between pt-2">
-          <div className="flex items-center space-x-3">
-            <input type="checkbox" id="terms" className="w-5 h-5" />
-            <p >Remember me</p>
-          </div>
-          <div>
-            <a href="#" className="text-blue-500 items-end">Forgot password?</a>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center py-3">
-          <button
-            type="submit"
-            className="w-full py-3 bg-green-500 text-white font-bold rounded-3xl text-xl">
-            Login
-          </button>
-          <div className="py-3">
-            <label htmlFor="terms" className="text-gray-700">
-              Don't have an account?{" "}
-              <Link to="/register" className="text-blue-500 underline">
-                Create account
-              </Link>
-            </label>
-          </div>
-        </div>
-
-      </form>
-      {error && <p className="text-red-500 mt-4">{error}</p>}
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 };
 
 export default Login;
---
+++ vite-project/src/assets/components/screen/EventForm.tsx
@@ -0,0 +1,371 @@
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import HeaderSection from "../section/HeaderSection";
+import HamburgerMenu from "../utils/HamBurgerMenu";
+import { useState } from "react";
+import SideBar from "../section/SideBar";
+import { GoogleMap, LoadScript, Autocomplete } from "@react-google-maps/api";
+const libraries = ["places"];
+const GOOGLE_MAPS_API_KEY = "AIzaSyD9HHvERObvfEK9kquxQSaMpvj2W465DTY";
+
+const EventForm = () => {
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const [location, setLocation] = useState("");
+
+  const handlePlaceSelect = (autocomplete) => {
+    const place = autocomplete.getPlace();
+    if (place.formatted_address) {
+      setLocation(place.formatted_address);
+    }
+  };
+  return (
+    <div className="app-container bg-gray-100 w-screen h-screen flex flex-col">
+      {/* Header */}
+      <HeaderSection />
+
+      {/* Sidebar & Main Layout */}
+      <div className="flex flex-grow bg-gray-100">
+        {/* Hamburger Menu Button */}
+        <HamburgerMenu
+          setSidebarVisible={setSidebarVisible}
+          isSidebarVisible={isSidebarVisible}
+        />
+
+        {/* Sidebar */}
+        <div
+          className={`fixed inset-y-0 left-0 z-20 w-64 bg-white shadow-md transform transition-transform duration-300 ${
+            isSidebarVisible ? "translate-x-0" : "-translate-x-full"
+          } lg:translate-x-0 lg:static`}
+        >
+          <SideBar />
+        </div>
+
+        <main
+          className={`flex flex-col p-3 w-full min-h-screen transition-all duration-300 ${
+            isSidebarVisible ? "lg:ml-64" : "ml-0"
+          }`}
+        >
+          <div className="flex-grow overflow-auto p-3 max-h-[calc(100vh-4rem)]">
+            <form className="flexflex-col space-y-6">
+              <div className="space-y-12">
+                <div className=" pb-2">
+                  <h2 className="text-base/7 font-semibold text-gray-900">
+                    Create a new event
+                  </h2>
+
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="eventName"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Name of Event
+                      </label>
+                      <div className="mt-2">
+                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                          <input
+                            id="eventName"
+                            name="eventName"
+                            type="text"
+                            placeholder="Event name"
+                            className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="eventName"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Date of Event
+                      </label>
+                      <div className="mt-2">
+                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                          <input
+                            id="eventDate"
+                            name="eventDate"
+                            type="date"
+                            placeholder="Event date"
+                            className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="eventLocation"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Location
+                      </label>
+                      <div className="mt-2">
+                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                          <input
+                            id="eventLocation"
+                            name="eventLocation"
+                            type="url"
+                            placeholder="Event Location"
+                            className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="about"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        About
+                      </label>
+                      <div className="mt-2">
+                        <textarea
+                          id="about"
+                          name="about"
+                          rows={3}
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                          defaultValue={""}
+                        />
+                      </div>
+                      <p className="mt-3 text-sm/6 text-gray-600">
+                        Write a few sentences about Event.
+                      </p>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="cover-photo"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Cover photo
+                      </label>
+                      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div className="text-center">
+                          <PhotoIcon
+                            aria-hidden="true"
+                            className="mx-auto size-12 text-gray-300"
+                          />
+                          <div className="mt-4 flex text-sm/6 text-gray-600">
+                            <label
+                              htmlFor="file-upload"
+                              className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500"
+                            >
+                              <span>Upload a file</span>
+                              <input
+                                id="file-upload"
+                                name="file-upload"
+                                type="file"
+                                className="sr-only"
+                              />
+                            </label>
+                            <p className="pl-1">or drag and drop</p>
+                          </div>
+                          <p className="text-xs/5 text-gray-600">
+                            PNG, JPG, GIF up to 10MB
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="border-b border-gray-900/10 pb-12">
+                  <h2 className="text-base/7 font-semibold text-gray-900">
+                    Hosted by
+                  </h2>
+
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="first-name"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        First name
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="first-name"
+                          name="first-name"
+                          type="text"
+                          autoComplete="given-name"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="last-name"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Last name
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="last-name"
+                          name="last-name"
+                          type="text"
+                          autoComplete="family-name"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Email address
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Phone number
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="phone"
+                          name="phone"
+                          type="tel"
+                          autoComplete="tel"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="country"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Country
+                      </label>
+                      <div className="mt-2 grid grid-cols-1">
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        >
+                          <option>Finland</option>
+                          <option>United States</option>
+                          <option>Canada</option>
+                          <option>Mexico</option>
+                        </select>
+                        <ChevronDownIcon
+                          aria-hidden="true"
+                          className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="street-address"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Street address
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="street-address"
+                          name="street-address"
+                          type="text"
+                          autoComplete="street-address"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2 sm:col-start-1">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        City
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="city"
+                          name="city"
+                          type="text"
+                          autoComplete="address-level2"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2">
+                      <label
+                        htmlFor="region"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        State / Province
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="region"
+                          name="region"
+                          type="text"
+                          autoComplete="address-level1"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2">
+                      <label
+                        htmlFor="postal-code"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        ZIP / Postal code
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="postal-code"
+                          name="postal-code"
+                          type="text"
+                          autoComplete="postal-code"
+                          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                  <button
+                    type="button"
+                    className="text-sm/6 font-semibold text-gray-900"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+export default EventForm;
