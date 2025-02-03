a3917cdc31c288ea77583730eae8257615e5f4c2
asmita143 <89544722+asmita143@users.noreply.github.com>
2025-02-03 11:29:45 +0200

    Fixed sidebar navigation

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
-import { BrowserRouter as Router } from 'react-router-dom';  // Import Router
-import RouteConfig from './route';  // Import RouteConfig (with no Router)
+import { BrowserRouter as Router } from "react-router-dom"; // Import Router
+import RouteConfig from "./route"; // Import RouteConfig (with no Router)
 
 const App = () => {
   return (
     <Router>
-      <RouteConfig />  {/* Only wrap RouteConfig here */}
+      <RouteConfig />
     </Router>
   );
 };
--- vite-project/src/route.tsx
+++ vite-project/src/route.tsx
@@ -1,5 +1,5 @@
 // src/route.tsx
-import { Route, Routes } from "react-router-dom"; // Import Routes and Route
+import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
 import Register from "./Register";
@@ -10,8 +10,6 @@ import EventForm from "./assets/components/screen/EventForm";
 const RouteConfig = () => {
   return (
     <Routes>
-      {" "}
-      {/* Just use Routes here without another Router */}
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
