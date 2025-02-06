26de277bc55acc8bf1b8fed603ac2c9b6677f3e1
prabind <prabin.dhakal@metropolia.fi>
2025-02-07 00:31:05 +0200

    added icons in sidebar

--- vite-project/src/assets/components/section/SideBar.tsx
+++ vite-project/src/assets/components/section/SideBar.tsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import EventIcon from '@mui/icons-material/Event';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import EventRepeatIcon from '@mui/icons-material/EventRepeat';
+import StarBorderPurple500Icon from '@mui/icons-material/StarBorderPurple500';
 
 const Sidebar: React.FC<{ showButton?: boolean }> = ({ showButton }) => {
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
 
   const sidebarItems = [
-    { label: "Home", isDropdown: false, path: "/home" },
+    { label: "Home", isDropdown: false, path: "/home", icon:HomeIcon },
     {
-      label: "Your Events",
+      label: "Events",
       isDropdown: true,
       dropdownItems: [
-        { label: "Accepted", path: "/events/accepted" },
-        { label: "Interested", path: "/events/interested" },
-        { label: "Past Events", path: "/events/past" },
+        { label: "Accepted", path: "/events/accepted", icon:EventAvailableIcon },
+        { label: "Interested", path: "/events/interested", icon:StarBorderPurple500Icon },
+        { label: "Past Events", path: "/events/past", icon:EventRepeatIcon},
       ],
+      icon:EventIcon
     },
-    { label: "Notifications", isDropdown: false, path: "/notifications" },
-    { label: "Settings", isDropdown: false, path: "/settings" },
-    { label: "Profile", isDropdown: false, path: "/profile" },
+    { label: "Notifications", isDropdown: false, path: "/notifications", icon:NotificationsActiveIcon},
+    { label: "Settings", isDropdown: false, path: "/settings", icon:SettingsIcon},
+    { label: "Profile", isDropdown: false, path: "/profile", icon:PersonOutlineIcon },
   ];
 
   const [openDropdown, setOpenDropdown] = useState<number | null>(null);
@@ -28,14 +37,14 @@ const Sidebar: React.FC<{ showButton?: boolean }> = ({ showButton }) => {
   };
 
   return (
-    <div className="w-64 bg-white shadow-md pt-16 pb-16 px-4 mt-12 b-black-100">
+    <div className="w-64 bg-white shadow-md px-4 h-screen pt-10 b-black-100">
       <div className="p-4">
         <h1 className="text-xl font-bold text-gray-900">Menu</h1>
         <ul className="mt-4 space-y-2">
           {sidebarItems.map((item, index) => (
             <li key={index}>
               <div
-                className={`px-4 py-2 flex items-center justify-between text-black hover:bg-gray-100 cursor-pointer transition duration-200 ${
+                className={`px-4 py-3 flex items-center gap-10 text-black hover:bg-gray-100 cursor-pointer transition duration-200 ${
                   item.isDropdown ? "relative" : ""
                 }`}
                 onClick={() => {
@@ -51,7 +60,9 @@ const Sidebar: React.FC<{ showButton?: boolean }> = ({ showButton }) => {
                   item.isDropdown ? `${item.label} dropdown` : item.label
                 }
               >
-                <span>{item.label}</span>
+                  {item.icon && <item.icon className="" />}
+                  <span>{item.label}</span>
+                
                 {item.isDropdown && (
                   <span
                     className={`transform transition-transform duration-200 ${
@@ -63,13 +74,14 @@ const Sidebar: React.FC<{ showButton?: boolean }> = ({ showButton }) => {
                 )}
               </div>
               {item.isDropdown && openDropdown === index && (
-                <ul className="ml-6 mt-1 space-y-1">
+                <ul className="ml-7 mt-1 space-y-1">
                   {item.dropdownItems?.map((dropdownItem, dropdownIndex) => (
                     <li
                       key={dropdownIndex}
-                      className="px-4 py-2 text-gray-700 hover:text-black hover:bg-gray-100 cursor-pointer transition duration-200"
+                      className="px-4 py-2 flex items-center gap-5 text-gray-700 hover:text-black hover:bg-gray-100 cursor-pointer transition duration-200"
                       onClick={() => navigate(dropdownItem.path)}
                     >
+                      {dropdownItem.icon && <dropdownItem.icon className="" />}
                       {dropdownItem.label}
                     </li>
                   ))}
